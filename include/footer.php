c23abd8f80b97c175584db765dfe4ab23cd2e080
jakepaulus <jakepaulus@7bf8daad-5c25-0410-9fb3-b75c15c31602>
2007-09-16 02:48:05 +0000

    Added version statement to footer.php
    git-svn-id: https://collate-network.googlecode.com/svn/trunk@41 7bf8daad-5c25-0410-9fb3-b75c15c31602

--- include/footer.php
+++ include/footer.php
@@ -4,8 +4,9 @@
 
     <div id="footer">
         <p>
-	Collate:Network is &copy; 2007 by its <a href="http://collate.info/">developers</a>. <br />
-	This software is licensed under the terms of  <a href="license.txt">The MIT License</a>.
+	Collate:Network is &copy; 2007 by its <a href="http://collate.info/">developers</a><br />
+	This software is licensed under the terms of  <a href="license.txt">The MIT License</a><br />
+	Version: <?php echo $COLLATE['settings']['version']; ?>
         </p>
     </div>
 
