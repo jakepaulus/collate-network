43f1e7efc1a53519b26b33610a32a9e620ebd1f9
jakepaulus <jakepaulus@7bf8daad-5c25-0410-9fb3-b75c15c31602>
2007-09-29 23:18:04 +0000

    git-svn-id: https://collate-network.googlecode.com/svn/trunk@55 7bf8daad-5c25-0410-9fb3-b75c15c31602

--- include/nav.php
+++ include/nav.php
@@ -17,5 +17,37 @@
 	  </ul>
     </div>
   </div>
+  <br />
+  <div class="left_nav">
+    <h3>Quick Search</h3>
+	<div class="inner_box">
+	  <form action="search.php" method="get">
+	  <ul>
+	    <li style="font-size: .8em;">Hostname:</li>
+		<li>
+		  <input type="hidden" name="op" value="search" />
+		  <input type="hidden" name="first" value="1" />
+		  <input type="hidden" name="second" value="name" />
+		  <input type="text" name="search" />
+		</li>
+		<li><input type="submit" value=" Go " /></li>
+	  </ul>
+	  </form>
+	  <form action="search.php" method="get">  
+	  <ul>
+	    <li style="font-size: .8em;">IP:</li>
+		<li>
+		  <input type="hidden" name="op" value="search" />
+		  <input type="hidden" name="first" value="1" />
+		  <input type="hidden" name="second" value="ip" />
+		  <input type="text" name="search" />		  
+		</li>
+		<li><input type="submit" value=" Go " /></li>
+	  </ul>
+	  </form>
+	</div>
+  </div>
+  
+  
 </div>
 
