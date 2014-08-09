503c6480c3ab5f681ae847a112d1d4028521abd6
Jake Paulus <jakepaulus@gmail.com>
2014-08-09 18:22:58 -0400

    Re-factored ACL code in resize_subnet() in subnets.php to fix an issue with ACLs not being resized properly while shrinking subnets.

--- subnets.php
+++ subnets.php
@@ -14,7 +14,7 @@ switch($op){
     break;
     
     case "submit";
-	AccessControl("3", null);
+    AccessControl("3", null);
     submit_subnet();
     break;
     
@@ -93,14 +93,14 @@ function add_subnet (){
        "</form>";
     
     echo "<div style=\"float: left; width: 45%; padding-left: 10px; border-left: 1px solid #000;\">\n";
-	
+    
     // Here we'll figure out what available space is left in the IP Block and list it out for the user
     $ipspace = array();
     
     $sql = "SELECT name, start_ip, end_ip FROM blocks WHERE id = '$block_id'";
     $results = mysql_query($sql);
     list($block_name,$block_long_start_ip,$block_long_end_ip) = mysql_fetch_row($results);
-	if(!empty($block_long_start_ip)){
+    if(!empty($block_long_start_ip)){
 
       array_push($ipspace, $block_long_start_ip);
       
@@ -148,14 +148,14 @@ function add_subnet (){
         }
       }
       echo "</table></div>\n".
-	       "<div id=\"spacesearch\" style=\"display: none;\">\n".
-		   "<p><a href=\"#\" onclick=\"new Effect.toggle('blockspace', 'blind', { delay: 0.1 }); \n".
-		   "   new Effect.toggle('spacesearch', 'blind', { delay: 0.1 }); return false;\">".$COLLATE['languages']['selected']['showblockspace']."</a></p>\n";
-	}
-	else{
-	  echo "<div id=\"spacesearch\">";
-	  $searchonlyparam = '&amp;searchonly=true';
-	}
+           "<div id=\"spacesearch\" style=\"display: none;\">\n".
+           "<p><a href=\"#\" onclick=\"new Effect.toggle('blockspace', 'blind', { delay: 0.1 }); \n".
+           "   new Effect.toggle('spacesearch', 'blind', { delay: 0.1 }); return false;\">".$COLLATE['languages']['selected']['showblockspace']."</a></p>\n";
+    }
+    else{
+      echo "<div id=\"spacesearch\">";
+      $searchonlyparam = '&amp;searchonly=true';
+    }
 
     echo "<h3>".$COLLATE['languages']['selected']['SearchIPSpace']."</h3><br />\n".
          "<p><b>".$COLLATE['languages']['selected']['Subnet'].":</b> <input id=\"subnetsearch\" type=\"text\"><br />".
@@ -252,24 +252,24 @@ function submit_subnet(){
   // Add static IP for the Default Gateway  
   if(!empty($gateway)){
     $long_gateway = ip2decimal($gateway);
-	$subnet_test = $long_gateway & $long_mask;
-	if($subnet_test !== $long_start_ip){
-	  mysql_query("ROLLBACK");
+    $subnet_test = $long_gateway & $long_mask;
+    if($subnet_test !== $long_start_ip){
+      mysql_query("ROLLBACK");
       $notice = 'invalidip';
       $guidance = urlencode($guidance);
       header("Location: subnets.php?op=add&block_id=$block_id&name=$name&ip=$ip&gateway=$gateway&acl_start=$acl_start&acl_end=$acl_end&note=$note&guidance=$guidance&notice=$notice");
       exit();
-	}
-	
+    }
+    
     $validate_gateway = validate_static_ip($gateway);
-	if($validate_gateway['0'] === false){
-	  mysql_query("ROLLBACK");
-	  $notice = $validate_gateway['error'];
-	  $guidance = urlencode($guidance);
-	  header("Location: subnets.php?op=add&block_id=$block_id&name=$name&ip=$ip&gateway=$gateway&acl_start=$acl_start&acl_end=$acl_end&note=$note&guidance=$guidance&notice=$notice");
+    if($validate_gateway['0'] === false){
+      mysql_query("ROLLBACK");
+      $notice = $validate_gateway['error'];
+      $guidance = urlencode($guidance);
+      header("Location: subnets.php?op=add&block_id=$block_id&name=$name&ip=$ip&gateway=$gateway&acl_start=$acl_start&acl_end=$acl_end&note=$note&guidance=$guidance&notice=$notice");
       exit();
-	}
-	
+    }
+    
     $sql = "INSERT INTO statics (ip, name, contact, note, subnet_id, modified_by, modified_at) 
            VALUES('$long_gateway', 'Gateway', 'Network Admin', 'Default Gateway', '$subnet_id', '$username', now())";
     mysql_query($sql);
@@ -282,8 +282,8 @@ function submit_subnet(){
   $message = "Subnet $name ($cidr) has been created";
   AccessControl($accesslevel, $message); // No need to generate logs when nothing is really happening. This 
                                          // goes down here where we know stuff has actually been written. Access
-										 // Control actually happened before submit_subnet() was called.
-										 
+                                         // Control actually happened before submit_subnet() was called.
+                                         
  
   $notice = "subnetadded-notice";
   header("Location: subnets.php?block_id=$block_id&notice=$notice");
@@ -367,7 +367,7 @@ function list_subnets(){
                ]); 
              }}); 
            };
-		   return false;\"><img src=\"./images/remove.gif\" alt=\"X\" title=\"".
+           return false;\"><img src=\"./images/remove.gif\" alt=\"X\" title=\"".
            $COLLATE['languages']['selected']['deletesubnet']."\" /></a>";
     }
     echo "</td>
@@ -458,8 +458,8 @@ function modify_subnet (){
       echo " <a href=\"_subnets.php?op=toggle_stale-scan&amp;subnet_id=$subnet_id&amp;toggle=off\" \">".
            "<img src=\"./images/scanning.png\" alt=\"\" title=\"".$COLLATE['languages']['selected']['disablestalescan']."\" /></a>";
     }
-	echo "</h3><br /><br />";
-	
+    echo "</h3><br /><br />";
+    
   $movesubnet = str_replace("%subnet_name%", $subnet_name, $COLLATE['languages']['selected']['Movesubnet']);
   echo "<h3>$movesubnet</h3><hr />\n".
        "<form action=\"subnets.php?op=submitmove\" method=\"post\">\n".
@@ -471,25 +471,25 @@ function modify_subnet (){
   $result = mysql_query($sql);
   while(list($select_block_id,$select_block_name,$select_block_parent) = mysql_fetch_row($result)){
     #$block_paths[$select_block_id]['name']=$select_block_name;
-	$block_paths[$select_block_id]="$select_block_name";
-	while($select_block_parent !== null){ #this has the potential to be really slow and awful...
-	  $recursive_result = mysql_query("SELECT name, parent_id FROM blocks WHERE id='$select_block_parent'");
-	  list($recursive_parent_name,$recursive_parent_parent) = mysql_fetch_row($recursive_result);
-	  $block_paths[$select_block_id] = "$recursive_parent_name/".$block_paths[$select_block_id];
-	  $select_block_parent = $recursive_parent_parent;
-	}
-	$block_paths[$select_block_id]='[root]/'.$block_paths[$select_block_id];
+    $block_paths[$select_block_id]="$select_block_name";
+    while($select_block_parent !== null){ #this has the potential to be really slow and awful...
+      $recursive_result = mysql_query("SELECT name, parent_id FROM blocks WHERE id='$select_block_parent'");
+      list($recursive_parent_name,$recursive_parent_parent) = mysql_fetch_row($recursive_result);
+      $block_paths[$select_block_id] = "$recursive_parent_name/".$block_paths[$select_block_id];
+      $select_block_parent = $recursive_parent_parent;
+    }
+    $block_paths[$select_block_id]='[root]/'.$block_paths[$select_block_id];
   }
   natcasesort($block_paths);
   foreach ($block_paths as $select_id => $select_text){
     if($select_block_parent == $select_id){
-	  echo "<option selected=\"selected\" value=\"$select_id\">$select_text</option>\n";
-	}
-	else{
-	  echo "<option value=\"$select_id\">$select_text</option>\n";
+      echo "<option selected=\"selected\" value=\"$select_id\">$select_text</option>\n";
+    }
+    else{
+      echo "<option value=\"$select_id\">$select_text</option>\n";
     }
   }
-	   
+       
   echo "</select></p>\n".
        "<p><input type=\"submit\" value=\" ".$COLLATE['languages']['selected']['Go']." \" /></p></form><br /><br />";
 
@@ -541,7 +541,7 @@ function resize_subnet() {
 
   $subnet_id = (isset($_POST['subnet_id']) && is_numeric($_POST['subnet_id'])) ? $_POST['subnet_id'] : '';
   $new_subnet = (isset($_POST['new_subnet'])) ? $_POST['new_subnet'] : '';
-  $confirm = (empty($_POST['confirm'])) ? '' : $_POST['confirm'];
+  $confirm = (isset($_POST['confirm'])) ? true : false;
 
   $sql = "SELECT name, start_ip, end_ip, mask, block_id FROM subnets WHERE id='$subnet_id'";
   $result = mysql_query($sql);
@@ -566,11 +566,11 @@ function resize_subnet() {
   $new_long_start_ip = $return['long_start_ip'];
   $new_end_ip = $return['end_ip'];
   $new_long_end_ip = $return['long_end_ip'];
-  $new_long_mask = $return['long_mask'];    
+  $new_long_mask = $return['long_mask'];
 
   $new_cidr=subnet2cidr($new_long_start_ip, $new_long_mask);
 
-  if($confirm != 'true'){
+  if($confirm === false){
     require_once('./include/header.php');
   }
   else {
@@ -590,25 +590,25 @@ function resize_subnet() {
     }
 
     #  * list static IP addresses that would be lost
-    if($confirm != 'true') {
+    if($confirm === false) {
       $sql = "SELECT id, ip, name, contact, note, failed_scans FROM statics WHERE 
              CAST(ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$original_long_mask' & 0xFFFFFFFF AS UNSIGNED) = 
-			 CAST('$original_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
+             CAST('$original_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
              AND CAST(ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$new_long_mask' & 0xFFFFFFFF AS UNSIGNED) != 
-			 CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
+             CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
              ORDER BY `ip` ASC";
     }
     else {
       $sql = "DELETE FROM statics WHERE
              CAST(ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$original_long_mask' & 0xFFFFFFFF AS UNSIGNED) = 
-			 CAST('$original_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
+             CAST('$original_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
              AND CAST(ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$new_long_mask' & 0xFFFFFFFF AS UNSIGNED) != 
-			 CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)";
+             CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)";
     }
     
     $result = mysql_query($sql);
     $totalrows = mysql_num_rows($result);
-    if($confirm != 'true'){
+    if($confirm === false){
       $staticstobedeleted = str_replace("%original_subnet_name%", $original_subnet_name, $COLLATE['languages']['selected']['staticstodelete']);
       echo "<h1>$staticstobedeleted:</h1><br />\n";
     }
@@ -626,7 +626,7 @@ function resize_subnet() {
         }
       echo "</table><br /><br />";
     }
-    elseif($confirm != 'true'){
+    elseif($confirm === false){
       echo "<p>".$COLLATE['languages']['selected']['nostaticsdeleted']."</p><br /><br />";
     }  
 
@@ -634,63 +634,67 @@ function resize_subnet() {
     # Find acls matching original subnet_id and see if start and end fall within new subnet
     $sql = "SELECT id, name, start_ip, end_ip FROM acl WHERE subnet_id='$subnet_id' AND (
            CAST(start_ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$new_long_mask' & 0xFFFFFFFF AS UNSIGNED) != 
-		   CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
+           CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
            OR CAST(end_ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$new_long_mask' & 0xFFFFFFFF AS UNSIGNED) != 
-		   CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED))";
+           CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED))";
     $result = mysql_query($sql);
-    if($confirm != 'true'){
+    if($confirm === false){
       $aclstobechanged = str_replace("%original_subnet_name%", $original_subnet_name, $COLLATE['languages']['selected']['aclstobechanged']);
       echo "<h1>$aclstobechanged:</h1><br />\n";
-    }
-    if (mysql_num_rows($result) == '0' && $confirm != 'true'){
-      echo "<p>".$COLLATE['languages']['selected']['noaclschanged']."</p><br /><br />"; 
-    }
-    elseif ($confirm != 'true'){
-      if($confirm != 'true'){ 
+
+      if(mysql_num_rows($result) == '0'){
+        echo "<p>".$COLLATE['languages']['selected']['noaclschanged']."</p><br /><br />"; 
+      }
+      else{
         echo "<table width=\"100%\">\n".
              "<tr><th>".$COLLATE['languages']['selected']['Name']."
              </th><th>".$COLLATE['languages']['selected']['StartingIP']."</th><th>".
              $COLLATE['languages']['selected']['EndIP']."</th><th>".$COLLATE['languages']['selected']['Modification']."</th></tr>\n".
              "<tr><td colspan=\"4\"><hr class=\"head\" /></td></tr>";
       }
-           
-      while(list($acl_id,$acl_name,$acl_long_start_ip,$acl_long_end_ip) = mysql_fetch_row($result)){
-        $note = ""; # this might not get set below.
-        $sql = "";
-
-        if(($acl_long_start_ip & $new_long_mask) == $new_long_start_ip) {
-          $new_acl_start_ip = long2ip($acl_long_start_ip);
-        }
-        else {
-          $new_acl_start_ip = $new_start_ip;
-          $note = "<b>".$COLLATE['languages']['selected']['StartingIPmodified']."</b>";
-          $sql = "UPDATE acl SET start_ip='$new_long_start_ip' WHERE id='$acl_id'";
-        }
-
-        if(($acl_long_end_ip & $new_long_mask) == $new_long_start_ip) {
-          $new_acl_end_ip = long2ip($acl_long_end_ip);
-        }
-        else {
-          $new_acl_end_ip = $new_end_ip;
-          $note = "<b>".$COLLATE['languages']['selected']['EndIPmodified']."</b>";
-          $sql = "UPDATE acl SET end_ip='$new_long_end_ip' WHERE id='$acl_id'";
-        }
-
-        if (($new_acl_start_ip == $new_start_ip) && ($new_acl_end_ip == $new_end_ip)){
-          $new_acl_start_ip = long2ip($acl_long_start_ip);
-          $new_acl_end_ip = long2ip($acl_long_end_ip);
-          $note = "<b>".$COLLATE['languages']['selected']['ToBeDeleted']."</b>";
-          $sql = "DELETE FROM acl WHERE id='$acl_id'";
-        }
-        if ($confirm != 'true') {
-          echo "<tr><td>$acl_name</td><td>$new_acl_start_ip</td><td>$new_acl_end_ip</td><td>$note</td></tr>\n";
-        }
-        elseif(strlen($sql) > '2') {
-          $result = mysql_query($sql);
-        }
+	}
+        
+    while(list($acl_id,$acl_name,$acl_long_start_ip,$acl_long_end_ip) = mysql_fetch_row($result)){
+      $note = ""; # this might not get set below.
+      $sql = "";
+    
+      if(($acl_long_start_ip & $new_long_mask) == $new_long_start_ip) {
+        $new_acl_start_ip = long2ip($acl_long_start_ip);
       }
-      if ($confirm != 'true') { echo "</table>\n"; }
+      else {
+        $new_acl_start_ip = $new_start_ip;
+        $note = "<b>".$COLLATE['languages']['selected']['StartingIPmodified']."</b>";
+        $sql = "UPDATE acl SET start_ip='$new_long_start_ip' WHERE id='$acl_id'";
+      }
+    
+      if(($acl_long_end_ip & $new_long_mask) == $new_long_start_ip) {
+        $new_acl_end_ip = long2ip($acl_long_end_ip);
+      }
+      else {
+        $new_acl_end_ip = $new_end_ip;
+        $note = "<b>".$COLLATE['languages']['selected']['EndIPmodified']."</b>";
+        $sql = "UPDATE acl SET end_ip='$new_long_end_ip' WHERE id='$acl_id'";
+      }
+    
+      if (($new_acl_start_ip == $new_start_ip) && ($new_acl_end_ip == $new_end_ip)){
+	    # we wouldn't generally have an ACL reserve a whole subnet. We'll just ditch the ACL
+		# and let the user make something new 
+        $new_acl_start_ip = long2ip($acl_long_start_ip);
+        $new_acl_end_ip = long2ip($acl_long_end_ip);
+        $note = "<b>".$COLLATE['languages']['selected']['ToBeDeleted']."</b>";
+        $sql = "DELETE FROM acl WHERE id='$acl_id'";
+      }
+      
+      if ($confirm === false) {
+        echo "<tr><td>$acl_name</td><td>$new_acl_start_ip</td><td>$new_acl_end_ip</td><td>$note</td></tr>\n";
+      }
+      elseif(!empty($sql)) {
+        $result = mysql_query($sql);
+      }
     }
+    if($confirm === false){
+	  echo "</table>\n";
+	}
   }
   else{
     # if larger:
@@ -702,7 +706,7 @@ function resize_subnet() {
     #  * list all subnets that new network overlaps
     $sql = "SELECT `id`, `name`, `start_ip`, `end_ip`, `mask`, `note` FROM `subnets` WHERE
             CAST(start_ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$new_long_mask' & 0xFFFFFFFF AS UNSIGNED) = 
-		    CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED) ORDER BY `start_ip` ASC";
+            CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED) ORDER BY `start_ip` ASC";
     $results = mysql_query($sql);
     
     $subnetstomerge = str_replace("%original_subnet_name%", $original_subnet_name, $COLLATE['languages']['selected']['subnetstomerge']);
@@ -738,7 +742,7 @@ function resize_subnet() {
       if($confirm != 'true'){ echo "</table>"; }
       else {
         $sql = "DELETE FROM `subnets` WHERE CAST(start_ip & 0xFFFFFFFF AS UNSIGNED) & CAST('$new_long_mask' & 0xFFFFFFFF AS UNSIGNED) = 
-		        CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
+                CAST('$new_long_start_ip' & 0xFFFFFFFF AS UNSIGNED)
                 AND id != '$subnet_id'";
         $result = mysql_query($sql);
       
