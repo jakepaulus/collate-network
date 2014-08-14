aec68297adf2e3b9cf48bf63903893cc02bbf222
Jake Paulus <jakepaulus@gmail.com>
2014-08-14 19:43:48 -0400

    Added version check to index.php to make install and upgrade easier.

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php 
 require_once('./include/common.php');
+if(!isset($COLLATE['settings']['version']) || $COLLATE['settings']['version'] != '2.2'){
+  header("Location: install.php");
+  exit();
+}
 require_once('./include/header.php');
 
   echo $COLLATE['languages']['selected']['indextext'];
-
 
 require_once('./include/footer.php'); 
 ?>
